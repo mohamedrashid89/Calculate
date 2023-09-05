63087a725caef167d48dccda33167c4b94127301
MOHAMED RASHID <131651353+mohamedrashid89@users.noreply.github.com>
2023-09-05 11:26:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// const display = document.querySelector(".display");
+// const buttons = document.querySelectorAll("button");
+// const operators = ["%", "*", "/", "-", "+", "="];
+// let output = "";
+
+// // Define function to calculate based on button clicked
+
+// const calculate = (btnValue) => {
+//     if(btnValue === "=" && output !== ""){
+//         // if output has '%', replace with '/100' before evaluating.
+//         output = eval(output.replace('%', '/100'));
+//     } else if(btnValue === "AC") {
+//         output = ""
+//     } else if(btnValue === "DEL") {
+//         // if DEL button is clicked, remove the last character from the output.
+//         output = output.toString().slice(0, -1);
+//     } else {
+//         // if output is empty and button is specialChars then return
+//         if(output === "" && operators.includes(btnValue)) return;
+//         output += btnValue;
+//     }
+//     display.value = output;
+// }
+
+
+// // Add event listener to buttons, call calculate() on click.
+// buttons.forEach((button) => {
+
+//     // Button click listener calls calculate() with dataset value as argument.
+//     button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+
+
+// })
+
+// what is tostring() ????
+
+// The toString () methods returns a string as string
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const operators = ["*", "/", "-", "%", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+    if(btnValue === "=" && output !== "") {
+        output = eval(output.replace('%', '/100'));
+    } else if(btnValue === "AC") {
+        output = ""
+    } else if(btnValue === "DEL") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if(output === "" && operators.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+}
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+})
